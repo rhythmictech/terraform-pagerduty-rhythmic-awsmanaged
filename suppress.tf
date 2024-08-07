e86a9cbb212468238302a21ad2f4529d538f6596
Cris Daniluk <cdaniluk@rhythmictech.com>
2024-08-07 11:50:59 -0400

    refine filter

--- suppress.tf
+++ suppress.tf
@@ -7,7 +7,7 @@ resource "pagerduty_event_orchestration_service" "account" {
     rule {
       label = "Suppress VPN Alerts"
       condition {
-        expression = "raw_event.details.log_sample matches part 'Connectivity on the second tunnel was not affected during this time'"
+        expression = "event.custom_details.body matches part 'Connectivity on the second tunnel was not affected during this time'"
       }
       actions {
         suppress = true
